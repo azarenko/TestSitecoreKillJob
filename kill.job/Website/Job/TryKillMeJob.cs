6745fad980afbf465150f89979cef19d9784b30c
Andrey <andrey.andreychenko@valtech.nl>
2020-08-14 16:33:49 +0300

    Job_add

---
+++ kill.job/Website/Job/TryKillMeJob.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Data.Items;
+using Sitecore.Tasks;
+using System.Threading;
+
+namespace Website.Job
+{
+    public class TryKillMeJob
+    {
+        public void Run(Item[] items, CommandItem command, ScheduleItem schedule)
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}

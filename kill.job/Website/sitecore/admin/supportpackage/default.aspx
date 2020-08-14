58301d44df00c960f10d2f7117c0994718f05246
sergey__azarenko <sergey.azarenko@valtech.com>
2020-08-14 11:58:59 +0300

    init sitecore prj

---
+++ kill.job/Website/sitecore/admin/supportpackage/default.aspx
@@ -0,0 +1,1 @@
+<%@ Page Language="C#" AutoEventWireup="true" Inherits="Sitecore.ExperienceContentManagement.Administration.Default" %>
---
+++ kill.job/Website/sitecore/login/login.js
@@ -0,0 +1,65 @@
+﻿// Note: Disabling and enabling to make bootstrap field validation work
+
+window.onload = function () {
+
+    var showRecovery = document.getElementsByClassName("show-recovery");
+     if (showRecovery != null && showRecovery.length != 0) {
+         showRecovery[0].onclick = function () {
+             document.getElementById("login").style.display = "none";
+             document.getElementById("passwordRecovery").style.display = "block";
+             var userNameForgotNode = document.getElementById("UserNameForgot");
+             if (userNameForgotNode != null) {
+                 userNameForgotNode.disabled = false;
+                 userNameForgotNode.focus();
+             }
+             document.getElementById("UserName").disabled = true;
+             document.getElementById("Password").disabled = true;
+         };
+     }
+     
+     var hideRecovery = document.getElementsByClassName("hide-recovery");
+     if (hideRecovery != null && hideRecovery.length != 0) {
+         hideRecovery[0].onclick = function () {
+             document.getElementById("passwordRecovery").style.display = "none";
+             document.getElementById("login").style.display = "block";
+             var userNameForgotNode = document.getElementById("UserNameForgot");
+             if (userNameForgotNode != null) {
+                 userNameForgotNode.disabled = true;
+             }
+             document.getElementById("UserName").disabled = false;
+             document.getElementById("Password").disabled = false;
+             document.getElementById("UserName").focus();
+         };
+     }
+
+    document.querySelector("#login input[type='submit']").onclick = function () {
+        if (document.getElementById("UserName").value === "" || document.getElementById("Password").value === "") {
+            document.getElementById("credentialsError").style.display = "block";
+            var loginFailedMessageNode = document.getElementById("loginFailedMessage");
+            if (loginFailedMessageNode != null) {
+                loginFailedMessageNode.style.display = "none";
+            }
+            return false;
+        } else {
+            document.getElementById("credentialsError").style.display = "none";
+        }
+    };
+
+    var licenseLinkNode = document.getElementById("licenseOptionsLink");
+
+    if (licenseLinkNode != null) {
+        licenseLinkNode.onclick = function () {
+            document.getElementById("login").style.display = "none";
+            document.getElementById("licenseOptions").style.display = "block";
+        };
+    }
+
+    var licenseBackNode = document.getElementById("licenseOptionsBack");
+
+    if (licenseBackNode != null) {
+        licenseBackNode.onclick = function () {
+            document.getElementById("licenseOptions").style.display = "none";
+            document.getElementById("login").style.display = "block";
+        };
+    }
+}
